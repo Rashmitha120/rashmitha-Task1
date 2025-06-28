778298e59ad2567f4ec6770e8b5dda1aef1ae6ae
Rashmitha120 <rashmithachiluveri@gmail.com>
2025-06-28 22:00:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showFact(place) {
+    const facts = {
+        "Goa": "Goa was a Portuguese colony until 1961!",
+        "Manali": "Manali is named after the Hindu lawgiver Manu.",
+        "Paris": "There are more dogs in Paris than children!"
+    };
+    alert(facts[place]);
+}
